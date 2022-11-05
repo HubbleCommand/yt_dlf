e39f3c40c591092cc69acb317f32aff3195d7b47
HubbleCommand <sasha.poirier@gmail.com>
2022-11-05 20:41:43 +0100

    Main bnb & etc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:yt_dlf/widgets/bottom_navigation_bar_page.dart';
+import 'package:yt_dlf/widgets/download_playlist.dart';
 
 void main() {
   runApp(const YTDLF());
@@ -27,19 +29,66 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static final List<BottomNavigationBarPage> _pages = <BottomNavigationBarPage>[
+    BottomNavigationBarPage(
+      bottomNavigationBarItem: const BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'Home',
+        backgroundColor: Colors.red,
+      ),
+      pageView: const DownloadPlaylistView(),
+    ),
+    BottomNavigationBarPage(
+      bottomNavigationBarItem: const BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'Home',
+        backgroundColor: Colors.red,
+      ),
+      pageView: const Text(
+        'Coming Soon',
+        style: optionStyle,
+      ),
+    ),
+    BottomNavigationBarPage(
+      bottomNavigationBarItem: const BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'Home',
+        backgroundColor: Colors.red,
+      ),
+      pageView: const Text(
+        'Coming Soon',
+        style: optionStyle,
+      ),
+    ),
+  ];
+
+  void _onBottomNavigationItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("widget.title"),
+        title: const Text('Youtube Download Flutter'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+        child: _pages.elementAt(_selectedIndex).pageView,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          for(BottomNavigationBarPage item in _pages)...[
+            item.bottomNavigationBarItem
           ],
-        ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onBottomNavigationItemTapped,
       ),
     );
   }

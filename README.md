d41632926bf4a69173b26dc3ff5328cea45f0a32
HubbleCommand <sasha.poirier@gmail.com>
2022-11-05 19:04:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# yt_dlf
+
+Flutter project to download youtube playlists. Basically yt-dl or yt-dlp but as an app.
---
+++ android/app/src/main/kotlin/com/hubble/yt_dlf/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hubble.yt_dlf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

5fba52c0bd98092d38825ad2bae635c841134136
HubbleCommand <sasha.poirier@gmail.com>
2022-11-06 15:39:15 +0100

    DownloadPlaylist show downloaded message

--- lib/utils/m3u.dart
+++ lib/utils/m3u.dart
@@ -5,6 +5,8 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
+
 class _PlaylistEntry {
   final String title;
   final String author;
@@ -25,28 +27,60 @@ class M3U {
   String source;
   final List<_PlaylistEntry> _entries = [];
 
-  M3U(this.source){
+  M3U(this.source, {bool readExisting = true}){
     File src = File(source);
-    if(src.existsSync()) {
-      //If the file already exists, we can read the data present
-      //List<String> lines = [];
-      //src.openRead().transform(utf8.decoder).transform(const LineSplitter()).forEach((line) { lines.add(line); });
 
-      //for(int i = 1)
-      src.openRead().transform(utf8.decoder).transform(const LineSplitter()).forEach((line) {
-        //if
-      });
+    if(readExisting) {
+      if(src.existsSync()) {
+        //If the file already exists, we can read the data present
+
+        String? title;
+        String? author;
+        src.openRead().transform(utf8.decoder).transform(const LineSplitter()).forEach((line) {
+          if(line.startsWith(headerFile)){
+            //continue;
+          } else if (line.startsWith(headerEntry)){
+            String valid = line.split(",")[1];
+            author = valid.split(" - ")[0];
+            title = valid.split(" - ")[1];
+          } else {
+            try {
+              debugPrint(line);
+              File src = File(line);
+
+              //Split filename to get title & author
+              String fileNameNoExtension = src.uri.pathSegments.last.split(".").first;
+
+              String titleLoc = title ?? fileNameNoExtension.split(" - ").first;
+              String authorLoc = author ?? fileNameNoExtension.split(" - ")[1];
+              _entries.add(_PlaylistEntry(title: titleLoc, author: authorLoc, source: line));
+            } on FileSystemException catch (_, e) {
+              //Not a valid file string...
+            } finally {
+              title = author = null;
+            }
+          }
+        });
+      }
     }
+
+    debugPrint(getAsString());
   }
 
   addEntry(String title, String author, String source) {
+    //If source already exists, don't add
+    for(var entry in _entries) {
+      if(entry.source == source) {
+        return;
+      }
+    }
     _entries.add(_PlaylistEntry(title: title, author: author, source: source));
   }
 
   String getAsString() {
-    String result = "$headerFile\n";
+    String result = "$headerFile\n\n";
     for(int i = 0; i < _entries.length - 1; i++) {
-      result += "$headerEntry:$i,${_entries[i].author} - ${_entries[i].title}\n";
+      result += "$headerEntry:$i, ${_entries[i].author} - ${_entries[i].title}\n";
       result += '${_entries[i].source}\n';
     }
     return result;

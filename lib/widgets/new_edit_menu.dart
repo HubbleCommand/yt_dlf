5783b0045bcbabeb9306ffb4fe873295f36a7dd5
HubbleCommand <sasha.poirier@gmail.com>
2022-11-07 20:13:02 +0100

    improved edit menu

--- lib/widgets/new_edit_menu.dart
+++ lib/widgets/new_edit_menu.dart
@@ -11,9 +11,10 @@ class NewEditPlaylistView extends StatefulWidget {
 }
 
 class _NewEditPlaylistViewState extends State<NewEditPlaylistView> {
+  static const _textStyle = TextStyle(color: Colors.white);
   M3U? data;
   String? outputFileName;
-  String? fileOutputDirectory;
+  String? outputDirectory;
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +23,7 @@ class _NewEditPlaylistViewState extends State<NewEditPlaylistView> {
       children: <Widget>[
         ElevatedButton(
           style: ElevatedButton.styleFrom(
-            minimumSize: const Size.fromHeight(50), // NEW
+            minimumSize: const Size.fromHeight(40), // NEW
           ),
           onPressed: () async {
             FilePickerResult? result = await FilePicker.platform.pickFiles(
@@ -45,30 +46,66 @@ class _NewEditPlaylistViewState extends State<NewEditPlaylistView> {
           },
           child: const Text("Edit Existing Playlist"),
         ),
-        const Text("Create new local playlist"),
-        Row(
-          children: [
 
-            const Text("Filename"),
-            Expanded(child: TextField(
-              cursorColor: outputFileName == null || outputFileName!.isEmpty ? Colors.red : Colors.black,
-              onChanged: (text) {
-                outputFileName = text;
-              },
-            ),),
-          ],
-        ),
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            minimumSize: const Size.fromHeight(50), // NEW
+        Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            border: Border.all(color: Colors.blue),
+            borderRadius: const BorderRadius.all(Radius.circular(10))
           ),
-          onPressed: () async {
-            String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
-            setState(() {
-              fileOutputDirectory = selectedDirectory;
-            });
-          },
-          child: const Text("Choose output directory")
+          child: Column(children: [
+            const Text("Create new local playlist", style: _textStyle),
+            Row(
+              children: [
+                const Text("Filename", style: _textStyle),
+                Expanded(child: TextField(
+                  style: _textStyle,
+                  onChanged: (text) {
+                    outputFileName = text;
+                  },
+                ),),
+              ],
+            ),
+            OutlinedButton(
+              onPressed: () async {
+                String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+                setState(() {
+                  outputDirectory = selectedDirectory;
+                });
+              },
+              child: const Text("Choose output directory", style: _textStyle)
+            ),
+            if(outputDirectory != null) ... [
+              Text("Output directory : $outputDirectory", style: _textStyle),
+            ],
+            OutlinedButton(
+                onPressed: () async {
+                  bool fileError = false;
+                  if(outputFileName != null && outputDirectory != null) {
+                    //Create file & return M3U
+                    try {
+                      debugPrint("Creating new .m3u at : $outputDirectory/$outputFileName.m3u");
+                      var m3u = M3U("$outputDirectory/$outputFileName.m3u", readExisting: false);
+                      data = m3u;
+                      widget.m3uCallback(data);
+                      return;
+                    } catch (e) {
+                      fileError = true;
+                    }
+                  }
+
+                  String message = '';
+                  if(outputFileName == null || outputDirectory == null) {
+                    message = "You must enter a filename and choose an output directory";
+                  } else if (fileError) {
+                    message = "Could not create file. Make sure you have a valid filename and path.";
+                  }
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message),));
+                },
+                child: const Text("Create", style: _textStyle)
+            ),
+          ],),
         ),
       ],
     );

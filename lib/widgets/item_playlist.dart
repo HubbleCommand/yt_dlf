e5b2075dab2f144ff719107d6010ff2af84022f4
HubbleCommand <sasha.poirier@gmail.com>
2022-11-09 22:10:09 +0100

    ~ item playlist changed layout

--- lib/widgets/item_playlist.dart
+++ lib/widgets/item_playlist.dart
@@ -24,40 +24,43 @@ class _PlaylistEntryViewState extends State<PlaylistEntryView> {
       appBar: AppBar(
         title: Text(entry.title),
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              const Text("Source"),
-              Text(entry.source, style: const TextStyle(color: Colors.grey),),
-              TextFormField(
-                enabled: false,
-                initialValue: entry.source,
-              ),
-              const Text("Author"),
-              TextFormField(
-                initialValue: entry.author,
-                onChanged: (value) => entry = PlaylistEntry(title: entry.title, author: value, source: entry.source),
-              ),
-              const Text("Title"),
-              TextFormField(
-                initialValue: entry.title,
-                onChanged: (value) => entry = PlaylistEntry(title: value, author: entry.author, source: entry.source),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size.fromHeight(50), // NEW
+      body: Column(
+        children: [
+          Expanded(child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                const Text("Source"),
+                Text(entry.source, style: const TextStyle(color: Colors.grey),),
+                TextFormField(
+                  enabled: false,
+                  initialValue: entry.source,
                 ),
-                onPressed: () {
-                  Navigator.pop(context, entry);
-                },
-                child: const Text("Save"),
-              ),
-            ],
+                const Text("Author"),
+                TextFormField(
+                  initialValue: entry.author,
+                  onChanged: (value) => entry = PlaylistEntry(title: entry.title, author: value, source: entry.source),
+                ),
+                const Text("Title"),
+                TextFormField(
+                  initialValue: entry.title,
+                  onChanged: (value) => entry = PlaylistEntry(title: value, author: entry.author, source: entry.source),
+                ),
+
+              ],
+            ),
+          ),),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size.fromHeight(50), // NEW
+            ),
+            onPressed: () {
+              Navigator.pop(context, entry);
+            },
+            child: const Text("Save"),
           ),
-        ),
-      ),
+        ],
+      )
     );
   }
 }
